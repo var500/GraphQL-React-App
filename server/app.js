e5323548f7b8e80f26841fac2f710d4548a0a420
var500 <varun5chodha@gmail.com>
2023-12-31 14:37:27 +0530

    Add: Cors

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { graphqlHTTP } = require('express-graphql');
 const schema = require("./schema/schema");
 const db = require("./config/db");
+const cors = require('cors');
 require('dotenv').config()
 
 db();
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
