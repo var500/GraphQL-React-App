60904f4f51acd66040146b660e3a44abc8d1e65c
var500 <varun5chodha@gmail.com>
2023-12-31 14:38:42 +0530

    Add: Binding GraphQL queries

--- client/src/app/ui/BookList.tsx
+++ client/src/app/ui/BookList.tsx
@@ -1,9 +1,24 @@
-export function BookList() {
-    return (
-        <div>
-            <ul id="book-list"></ul>
-            <li>Book Name</li>
-        </div>
-    );
+import { gql, useQuery } from "@apollo/client";
+
+const getBooksquery = gql`
+  query books {
+    books {
+      name
+      id
+    }
   }
-  
+`;
+
+export function BookList() {
+  const { loading, error, data } = useQuery(getBooksquery);
+    console.log(data)
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  return (
+    <div>
+      <ul id="book-list">
+        <li>Book Name</li>
+      </ul>
+    </div>
+  );
+}
