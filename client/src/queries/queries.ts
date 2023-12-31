99492cb2f2d882df99047852fffd9f82e7266db5
var500 <varun5chodha@gmail.com>
2024-01-01 04:29:34 +0530

    Add: Get Book Query

--- client/src/queries/queries.ts
+++ client/src/queries/queries.ts
@@ -19,12 +19,31 @@ const getAuthorsquery = gql`
 `;
 
 const addBookMutation = gql`
-  mutation addBook($name: String!, $genre: String!, $authorId: ID!){
-    addBook(name:$name,genre:$genre,authorId:$authorId){
+  mutation addBook($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
       name
       id
     }
   }
 `;
 
-export { getBooksquery, getAuthorsquery, addBookMutation };
+// getting a single book
+const getBookquery = gql`
+  query book($id: ID) {
+    book(id: $id) {
+      id
+      name
+      genre
+      author{
+        name
+        age
+        book {
+          name
+          id
+        }
+      }
+    }
+  }
+`;
+
+export { getBooksquery, getAuthorsquery, addBookMutation, getBookquery };
