34124b5688b0fc478e8e003439fdd84e89d464ad
var500 <varun5chodha@gmail.com>
2024-01-02 02:22:51 +0530

    Update: Book Details Logic and UI

--- client/src/components/BookDetails.tsx
+++ client/src/components/BookDetails.tsx
@@ -30,33 +30,45 @@ export function BookDetails(bookId: BookDetailsProps) {
     },
   });
 
-  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   // Access the data returned by the query
   const bookData: BookDetailsResponse = data?.book;
-
+  console.log(bookData);
   return (
-    <div id="book-details">
-      {bookData && (
-        <div className="mt-8">
-          <h1 className="text-2xl font-extrabold">{bookData.name}</h1>
-          <div className="flex flex-row items-center">
-            <p className="font-bold">{"Genre:"}</p>
-            <p>{bookData.genre}</p>
+    <div
+      id="book-details"
+      className="fixed top-0 right-0 w-2/5 h-full bg-red-400 overflow-auto p-8 shadow-lg box-border text-white"
+    >
+      {bookData ? (
+        bookData && (
+          <div className="mt-8">
+            <h1 className="text-2xl font-extrabold">{bookData.name}</h1>
+            <div className="flex flex-row items-center py-3">
+              <p className="font-bold">{"Genre:"}</p>
+              <p className="ml-2">{bookData.genre}</p>
+            </div>
+            <div className="flex flex-row items-center py-3">
+              <p className="font-bold">{"Author:"}</p>
+              <p className="ml-2">{bookData.author && bookData.author.name}</p>
+            </div>
+            <p className="py-3">{`All Books by ${bookData.author.name}:`}</p>
+            <ul className="mx-8 list-disc">
+              {bookData.author &&
+                bookData.author.books.map((item: Books) => (
+                  <li key={item.id} className="py-2">{item.name}</li>
+                ))}
+            </ul>
           </div>
-          <div className="flex flex-row items-center">
-            <p className="font-bold">{"Author:"}</p>
-            <p>{bookData.author && bookData.author.name}</p>
+        )
+      ) : loading ? (
+        loading && (
+          <div>
+            <p>Loading ...</p>
           </div>
-          <p >{`All Books by ${bookData.author.name}:`}</p>
-          <ul className="mx-8 list-disc">
-            {bookData.author &&
-              bookData.author.books.map((item: Books) => (
-                <li key={item.id}>{item.name}</li>
-              ))}
-          </ul>
-        </div>
+        )
+      ) : (
+        <p>No Book Selected</p>
       )}
     </div>
   );
